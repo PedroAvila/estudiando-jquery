600cdbe25c5b42b7650f1920fe99aa0db4102b08
PedroAvila <minimainor_austin@hotmail.com>
2021-04-28 16:16:18 -0500

    Encadenar métodos y recorrer el DOM con jQuery

---
+++ JuanAndresNuñez/vanilla-javascript/8-encadenar/js/main.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function () {
+    //$("section.contenido p:first-of-type");
+    //$(".contenido").find("p").last().prev().next().css("background-color", "red");
+    $(".contenido").find("p").last().prev().next().fadeOut();
+});
