6deda2f1502caafe88c91bd6ad066ef1358e05a9
PedroAvila <minimainor_austin@hotmail.com>
2021-04-30 18:13:12 -0500

    Interacción en el DOM con jQuery

---
+++ JuanAndresNuñez/vanilla-javascript/12-interaccion/js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    // Escuchar un <a>
+    $(".lateral").find("a").last().on("click", function(){
+        $(".contenido").children("p").css("color", "red");
+    });
+
+    //Escuchar los <p>
+    $(".contenido").children("p").on("click", function(evento){
+        evento.preventDefault();
+        console.log(this);
+    });
+});
