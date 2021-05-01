a72561c0c722ae04a412dab51ad8270af15e42ba
PedroAvila <minimainor_austin@hotmail.com>
2021-05-01 07:57:59 -0500

    This y jQuery

---
+++ JuanAndresNuñez/vanilla-javascript/13-this/js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    // Escuchar un <a>
+    $(".lateral").find("a").last().on("click", function(){
+        $(".contenido").children("p").css("color", "red");
+    });
+
+    //Escuchar los <p>
+    $(".contenido").children("p").on("click", function(evento){
+        evento.preventDefault();
+        //console.log(this);
+        $(this).css("color", "red");
+    });
+});
